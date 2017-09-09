04aa23b5cf084c5604e1af2ba6b34ef4f2fec9f8
genagain <ohta.g@husky.neu.edu>
2017-09-09 15:41:13 -0400

    add user input in response for testing

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from twilio.twiml.messaging_response import MessagingResponse, Message
 
 app = Flask(__name__)
@@ -6,8 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_monkey():
     """Respond to incoming texts with a simple text message and store said text in a database."""
+    body = request.values.get('Body', None).lower()
     resp = MessagingResponse()
-    msg = Message().body("Hello, Mobile Monkey").media("https://demo.twilio.com/owl.png")
+    msg = Message().body('Hi' + body).media("https://demo.twilio.com/owl.png")
     resp.append(msg)
     return str(resp)
 
