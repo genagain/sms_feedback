0a62ad439508ee2a38751964a638341ef26420b9
genagain <ohta.g@husky.neu.edu>
2017-09-09 17:03:49 -0400

    add thank you message in response to feedback

--- app/server.py
+++ app/server.py
@@ -8,7 +8,8 @@ def hello_monkey():
     """Respond to incoming texts with a simple text message and store said text in a database."""
     body = request.values.get('Body', None).lower()
     resp = MessagingResponse()
-    msg = Message().body('Hi' + body).media("https://demo.twilio.com/owl.png")
+    # TODO have a number of GIFs that are selected randomly for the media part
+    msg = Message().body('Thank you for your feedback! - Gen').media("https://demo.twilio.com/owl.png")
     resp.append(msg)
     return str(resp)
 
